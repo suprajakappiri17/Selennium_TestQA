4ce945ee9e375631afe3e051153b1a62c901da4e
supraja <supraja123>
2023-12-15 18:22:42 +0530

    commiting 15 dec code for testNG

---
+++ MavenTest/src/test/java/testNGpractice/NewTest.java
@@ -0,0 +1,22 @@
+package testNGpractice;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("method1");
+  }
+  public void e() {
+	  System.out.println("method1");
+  }
+
+public void g() {
+	  System.out.println("method1");
+}
+
+public void h() {
+	  System.out.println("method1");
+}
+}
+
---
+++ MavenTest/src/test/java/testNGpractice/Test1.java
@@ -0,0 +1,46 @@
+package testNGpractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	WebDriver driver;
+
+  @Test
+  public void testcase1() throws InterruptedException {
+		System.setProperty("webdriver.edge.driver", "E:\\msedgedriver.exe");
+		 driver = new EdgeDriver();
+		driver.get("https://www.facebook.com/login.php");  
+		Thread.sleep(5000);
+  }
+  
+  @Test
+  public void testcase2() throws InterruptedException {
+	  String title = driver.getTitle(); // String title=facebook
+		System.out.println(title); 
+		Thread.sleep(5000);
+  }
+  
+  @Test
+  public void testcase3() throws InterruptedException {
+	  driver.manage().window().maximize();
+		Thread.sleep(5000);
+	  
+  }
+  
+  
+  @Test
+  public void testcase4() throws InterruptedException {
+	  driver.manage().window().minimize();
+		Thread.sleep(5000);
+	  
+  }
+  
+  @Test
+  public void testcase5() {
+		driver.close();
+	  
+  }
+}
+
